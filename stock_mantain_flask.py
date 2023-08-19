82e592d87b7591779440f40590d873bad82026c6
Nikhil <nikhilbonageri68@gmail.com>
2023-08-20 01:16:30 +0530

    multile gorden added

--- stock_mantain_flask.py
+++ stock_mantain_flask.py
@@ -6,8 +6,12 @@ from datetime import datetime
 
 def establishMongoConnectionAndgetDb():
     client = MongoClient("mongodb+srv://vps:vps123@cluster0.tpkcrim.mongodb.net/?retryWrites=true&w=majority")
-    db = client.get_database('VPS')
-    return  db
+    return  client
+def getDb(client,dbName):
+    if dbName is None:
+       return client.get_database("VPS")
+    return client.get_database(dbName)
+
 def emptyCheck(arr):
     print("checking empty Values",arr)
     for elem in  arr.values():
@@ -15,7 +19,13 @@ def emptyCheck(arr):
             print("Empty value Found",elem)
             return True
     return  False
-
+def getGdName(dbName):
+    if dbName == "VPS2":
+        return "SINGRI"
+    elif dbName == "VPS3":
+        return "Gorden 3"
+    else:
+        return "CAR SHED"
 def getAllItemsFromDB(db):
     db.stock.create_index("uid")
     db.itemDelivery.create_index("uid")
@@ -25,7 +35,7 @@ def saveItemTodb(db,obj):
 def saveDeliveryEntryToDb(db, obj):
     return db.itemDelivery.insert_one(obj);
 def getAllDeliveriesForItem(db,item):
-    return db.itemDelivery.find({"uid":item},{"_id":0});
+    return db.itemDelivery.find({"uid":item},{"_id":0}).sort("date",-1);
 def updateDecQty(db,item,qty):
     item =int(item)
     return db.stock.update_one({"uid": item}, {"$inc":{"qty":qty}});
@@ -36,58 +46,76 @@ def updateItem(db,uid,item, itemDisc):
      uid =int(uid)
      return db.stock.update_one({"uid": uid}, {"$set":{"item":item,"itemDescription":itemDisc}});
 
-db = establishMongoConnectionAndgetDb()
-records = db.stock
-print(records.count_documents({}))
+client = establishMongoConnectionAndgetDb()
+
 
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
 
 
+@app.after_request
+def add_header(response):
+    response.cache_control.no_store = True
+    return response
+
+@app.route('/favicon.ico')
+def favicon():
+    return jsonify({"success": "NULL."}), 204
 
 @app.route('/')
-def hello_world():
-    return render_template('template.html',items=getAllItemsFromDB(db),selectedItem="",itemDeliveries =[])
+@app.route('/<dbName>')
+def hello_world(dbName='VPS'):
+    db = getDb(client,dbName)
+    return render_template('template.html',items=getAllItemsFromDB(db),selectedItem="",itemDeliveries =[],gd =getGdName(dbName))
+
 
 @app.route('/saveItem', methods=['PUT'])
-def save_item():
+@app.route('/saveItem/<dbName>', methods=['PUT'])
+def save_item(dbName):
       try:
         data = request.get_json()  # Get the JSON data from the request
         if emptyCheck(data):
             return  jsonify({"error": "Empty fields."}), 500
+        db = getDb(client, dbName)
         data['uid'] = findAndModSeq(db)
         saveItemTodb(db,data)
+        print("Item Saved Successfully")
         return jsonify({"success": "Successfully Inserted."}), 200
       except Exception as e:
           print("Exception occured while saving Item",e)
           return jsonify({"error": "An error occurred. Please try again later."}), 500
 
+@app.route('/saveDeliveryEntry/<dbName>', methods=['PUT'])
 @app.route('/saveDeliveryEntry', methods=['PUT'])
-def save_deliveryEntry():
+def save_deliveryEntry(dbName):
       try:
         data = request.get_json()  # Get the JSON data from the request
         if emptyCheck(data):
             return  jsonify({"error": "Empty fields."}), 500
+        db = getDb(client,dbName)
         saveDeliveryEntryToDb(db,data)
         print(data)
         updateDecQty(db,data['uid'],int(data['qty'])*-1)
-        print("Exception occured while saving Delivery Entry", e)
         return jsonify({"success": "Successfully Inserted."}), 200
+        print("Delivery Entry Saved Successfully")
       except Exception as e:
+          print("Exception occured while saving Delivery Entry", e)
           return jsonify({"error": "An error occurred. Please try again later."}), 500
 
-@app.route('/getDeliveryEntries/<item>')
-def get_itemDeliveries(item):
-    print(list(getAllDeliveriesForItem(db,item)))
+@app.route('/getDeliveryEntries/<dbName>/<item>')
+def get_itemDeliveries(dbName,item):
+    db = getDb(client,dbName)
     return jsonify({"item":item,"itemDeliveries":list(getAllDeliveriesForItem(db,item))}), 200
 
+@app.route('/editStockQty/<dbName>', methods=['PUT'])
 @app.route('/editStockQty', methods=['PUT'])
-def edit_stock_qty():
+def edit_stock_qty(dbName):
       try:
         data = request.get_json() # Get the JSON data from the request)
         if emptyCheck(data):
             return  jsonify({"error": "Empty fields."}), 500
+        db = getDb(client,dbName)
         updateDecQty(db,data['uid'],int(data['qty']))
         reqObj = {}
         reqObj['uid'] = str(data['uid'])
@@ -106,14 +134,16 @@ def edit_stock_qty():
           print("Exception occured while Editing Qty", e)
           return jsonify({"error": "An error occurred. Please try again later."}), 500
 
-
+@app.route('/editItem/<dbName>', methods=['PUT'])
 @app.route('/editItem', methods=['PUT'])
-def edit_item():
+def edit_item(dbName):
     try:
         data = request.get_json()  # Get the JSON data from the request)
         if emptyCheck(data):
             return  jsonify({"error": "Empty fields."}), 500
+        db = getDb(client,dbName)
         updateItem(db, data['uid'], data['item'],data['itemDisc'])
+        print("Edited Item Successfully")
         return jsonify({"success": "Successfully Updated Quantity."}), 200
     except Exception as e:
         print("Exception occured while Editing Item", e)
